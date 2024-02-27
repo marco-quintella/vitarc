d8c96ced71502dd1596ef4867114ca63f27968fd
Marco Quintella <2om.marco.quintella@gmail.com>
2024-02-27 18:52:22 -0300

    Update uno.config.ts with new presets and transformers

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,18 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { presetScrollbar } from 'unocss-preset-scrollbar'
+import { presetForms } from '@julr/unocss-preset-forms'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts(),
+    presetScrollbar(),
+    presetForms(),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })
